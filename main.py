be603a2c0fc6dadc573cf4c2717009cc073052fb
JNasri <yusof200022@gmail.com>
2026-06-20 09:39:33 +0200

    2.2: Implement the code requirements to print Semantic Versioning

---
+++ main.py
@@ -0,0 +1,17 @@
+# HiveBox - A versatile application framework for
+# building modular and scalable applications.
+# @author: Youssef Nasri
+
+
+# Version follows Semantic Versioning (SemVer)
+__version__ = "0.0.1"
+
+# Main function to run the application
+def main():
+    # Display the application version and exit
+    print(f"HiveBox App Version: v{__version__}")
+
+
+# Entry point of the application
+if __name__ == "__main__":
+    main()
